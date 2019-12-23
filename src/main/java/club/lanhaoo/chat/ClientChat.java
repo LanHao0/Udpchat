1cb47ecfb69efb14a0e4aed1422215283cb05b37
LanHao <a913538526@gmail.com>
2019-12-23 11:11:19 +0800

    修改输入密码网页 兼容更多设备,
    整理文件夹

--- src/main/java/club/lanhaoo/chat/ClientChat.java
+++ src/main/java/club/lanhaoo/chat/ClientChat.java
@@ -9,6 +9,8 @@ package club.lanhaoo.chat; /**
 
 import club.lanhaoo.chat.HttpFileShare.App;
 import com.google.gson.Gson;
+import com.intellij.uiDesigner.core.GridConstraints;
+import com.intellij.uiDesigner.core.GridLayoutManager;
 import fi.iki.elonen.NanoHTTPD;
 
 import javax.swing.*;
@@ -37,32 +39,32 @@ public class ClientChat {
     private JButton serverIPButton;
     private JButton fileShareButton;
 
-    public static void main(String[] args) throws NullPointerException{
+    public static void main(String[] args) {
 
 
-        final UserSettings userSettings=new UserSettings();
+        final UserSettings userSettings = new UserSettings();
 
         JFrame frame = new JFrame("ClientChat");
 
-        ClientChat clientChat=new ClientChat();
+        ClientChat clientChat = new ClientChat();
 
-        JPanel jPanel=clientChat.panel1;
-        final JTextArea jTextArea_chat=clientChat.textArea1_chat;
-        final JTextArea jTextArea_message=clientChat.textArea_message;
+        JPanel jPanel = clientChat.panel1;
+        final JTextArea jTextArea_chat = clientChat.textArea1_chat;
+        final JTextArea jTextArea_message = clientChat.textArea_message;
 
-        final JList jList_iplist=clientChat.list1;
-        JButton jButton_send=clientChat.sendButton;
+        final JList jList_iplist = clientChat.list1;
+        JButton jButton_send = clientChat.sendButton;
 
-        JScrollPane jScrollPane=clientChat.scrollPane1;
+        JScrollPane jScrollPane = clientChat.scrollPane1;
 
-        JToolBar jToolBar=clientChat.Jtoolbar;
+        JToolBar jToolBar = clientChat.Jtoolbar;
         jToolBar.setFloatable(false);
 //        阻止移动
 
         frame.setContentPane(jPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-       //设置居中
+        //设置居中
         Point point = GraphicsEnvironment.getLocalGraphicsEnvironment().getCenterPoint();
         frame.setBounds(point.x - 600 / 2, point.y - 400 / 2, 600, 400);
 
@@ -71,7 +73,7 @@ public class ClientChat {
         jTextArea_message.grabFocus();
         //获取焦点
 
-        JButton jButton_severIP=clientChat.serverIPButton;
+        JButton jButton_severIP = clientChat.serverIPButton;
         jButton_severIP.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) {
 
@@ -95,9 +97,8 @@ public class ClientChat {
         });
 
 
-
         //回车监听
-        JButton jButton_HideIp=clientChat.hideIPButton;
+        JButton jButton_HideIp = clientChat.hideIPButton;
         jButton_HideIp.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) {
 
@@ -105,10 +106,10 @@ public class ClientChat {
 
             public void mousePressed(MouseEvent e) {
                 userSettings.setHidemyIp(!userSettings.getHidemyIp());
-                if (userSettings.getHidemyIp()){
+                if (userSettings.getHidemyIp()) {
                     jTextArea_chat.append("已设置隐藏ip, 若已设置昵称 需要重新设置昵称\n");
                     userSettings.setUserName(null);
-                }else{
+                } else {
                     jTextArea_chat.append("已显示ip\n");
                 }
             }
@@ -126,7 +127,7 @@ public class ClientChat {
             }
         });
 
-        JButton jButton_Nickname=clientChat.nicknameButton;
+        JButton jButton_Nickname = clientChat.nicknameButton;
         jButton_Nickname.addMouseListener(new MouseListener() {
             public void mouseClicked(MouseEvent e) {
 
@@ -134,7 +135,7 @@ public class ClientChat {
 
             public void mousePressed(MouseEvent e) {
                 userSettings.setUserName(JOptionPane.showInputDialog("输入自定义昵称"));
-                jTextArea_chat.append("已设置昵称"+userSettings.getUserName()+"\n");
+                jTextArea_chat.append("已设置昵称" + userSettings.getUserName() + "\n");
             }
 
             public void mouseReleased(MouseEvent e) {
@@ -149,7 +150,7 @@ public class ClientChat {
 
             }
         });
-        final JButton jButton_fileShare=clientChat.fileShareButton;
+        final JButton jButton_fileShare = clientChat.fileShareButton;
 
         jButton_fileShare.addMouseListener(new MouseListener() {
             @Override
@@ -161,13 +162,13 @@ public class ClientChat {
             public void mousePressed(MouseEvent e) {
                 try {
 
-                    App app= new App();
+                    App app = new App();
 
                     app.setWebpassword(JOptionPane.showInputDialog("设置密码?"));
 
-                    app.start(NanoHTTPD.SOCKET_READ_TIMEOUT,false);
+                    app.start(NanoHTTPD.SOCKET_READ_TIMEOUT, false);
 
-                    jTextArea_chat.append("开始分享文件,ip地址: "+ InetAddress.getLocalHost().getHostAddress()+":8089");
+                    jTextArea_chat.append("开始分享文件,ip地址: " + InetAddress.getLocalHost().getHostAddress() + ":8089");
                     jButton_fileShare.setText("停止分享文件");
 
 
@@ -195,10 +196,10 @@ public class ClientChat {
 
 
         jTextArea_chat.append("开始客户端，将使用端口2113\n");
-        final String serverIP=JOptionPane.showInputDialog("服务器地址");
+        final String serverIP = JOptionPane.showInputDialog("服务器地址");
 
         userSettings.setServerIp(serverIP);
-        jTextArea_chat.append("将使用 " +serverIP +" 作为服务器地址\n");
+        jTextArea_chat.append("将使用 " + serverIP + " 作为服务器地址\n");
 
 
         //        发送按钮监听
@@ -208,19 +209,19 @@ public class ClientChat {
             }
 
             public void mousePressed(MouseEvent e) {
-                String pure_message= jTextArea_message.getText();
+                String pure_message = jTextArea_message.getText();
 //
 //                if (setname){
 //                    raw_Data="[with_name]"+username+"@"+raw_Data;
 //
 //                }
 
-                long mtime= new Date().getTime();
-                Message message=new Message("text","",pure_message,String.valueOf(mtime));
+                long mtime = new Date().getTime();
+                Message message = new Message("text", "", pure_message, String.valueOf(mtime));
 
-                if (message.send(userSettings)){
+                if (message.send(userSettings)) {
                     jTextArea_message.setText(null);
-                }else {
+                } else {
                     jTextArea_chat.append("发送失败\n");
                 }
 
@@ -247,17 +248,17 @@ public class ClientChat {
             }
 
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode()== KeyEvent.VK_ENTER){
+                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
 
-                    String pure_message= jTextArea_message.getText();
+                    String pure_message = jTextArea_message.getText();
 
-                    long mtime= new Date().getTime();
-                    Message message=new Message("text","",pure_message,String.valueOf(mtime));
+                    long mtime = new Date().getTime();
+                    Message message = new Message("text", "", pure_message, String.valueOf(mtime));
                     message.setFromIp(Server.getIpAddress());
 
-                    if (message.send(userSettings)){
+                    if (message.send(userSettings)) {
                         jTextArea_message.setText(null);
-                    }else {
+                    } else {
                         jTextArea_chat.append("发送失败\n");
                     }
 
@@ -271,74 +272,72 @@ public class ClientChat {
         });
 
 
-
 //        接收服务器数据
-        try{
+        try {
             jTextArea_chat.append("开始接收服务器数据\n=============\n");
-            DatagramSocket datagramSocket=new DatagramSocket(12251);
-            byte[] bytes_from_server=new byte[1024];
-            DatagramPacket datagramPacket=new DatagramPacket(bytes_from_server,bytes_from_server.length);
+            DatagramSocket datagramSocket = new DatagramSocket(12251);
+            byte[] bytes_from_server = new byte[1024];
+            DatagramPacket datagramPacket = new DatagramPacket(bytes_from_server, bytes_from_server.length);
 
 
-            ArrayList arr_ip=new ArrayList();
-            final ListModel listModel_ip=new DefaultListModel();
+            ArrayList arr_ip = new ArrayList();
+            final ListModel listModel_ip = new DefaultListModel();
             jList_iplist.setModel(listModel_ip);
             //设置list模型
             jList_iplist.addMouseListener(new MouseAdapter() {
                 @Override
                 public void mouseClicked(MouseEvent e) {
                     super.mouseClicked(e);
-                    if (e.getClickCount()==2){
-                        int list_index=jList_iplist.locationToIndex(e.getPoint());
-                        System.out.println("点击的是"+ list_index);
-                        SingleTalk singleTalk=new SingleTalk();
+                    if (e.getClickCount() == 2) {
+                        int list_index = jList_iplist.locationToIndex(e.getPoint());
+                        System.out.println("点击的是" + list_index);
+                        SingleTalk singleTalk = new SingleTalk();
                         System.out.println(((DefaultListModel) listModel_ip).get(list_index));
-                        singleTalk.openWindow(((DefaultListModel) listModel_ip).get(list_index).toString(),userSettings);
+                        singleTalk.openWindow(((DefaultListModel) listModel_ip).get(list_index).toString(), userSettings);
                     }
                 }
             });
 
-            JScrollBar jScrollBar_chat=jScrollPane.getVerticalScrollBar();
+            JScrollBar jScrollBar_chat = jScrollPane.getVerticalScrollBar();
 
-            while (true){
+            while (true) {
                 datagramSocket.receive(datagramPacket);
                 //todo 接受到的消息都是来自服务端的。。。。
 
-                String message_pure=new String(datagramPacket.getData(),0,datagramPacket.getLength(),"UTF-8");
+                String message_pure = new String(datagramPacket.getData(), 0, datagramPacket.getLength(), "UTF-8");
 
-                Gson gson=new Gson();
-                Message message=gson.fromJson(message_pure,Message.class);
+                Gson gson = new Gson();
+                Message message = gson.fromJson(message_pure, Message.class);
 
-                String command=message.getCommand();
+                String command = message.getCommand();
                 if (!arr_ip.contains(message.getFromIp())) {
                     arr_ip.add(message.getFromIp());
                     ((DefaultListModel) listModel_ip).addElement(message.getFromIp());
                 }
 
 
-                if(command.contains("UserCommand")){
+                if (command.contains("UserCommand")) {
 
-                        jTextArea_chat.append("来自 "+message.getSender()+"\n");
-                        jTextArea_chat.append(message.getContent()+"\n");
-                        continue;
+                    jTextArea_chat.append("来自 " + message.getSender() + "\n");
+                    jTextArea_chat.append(message.getContent() + "\n");
+                    continue;
 
                 }
 
 
-                jTextArea_chat.append("来自 "+message.getFromIp()+"\n");
-                jTextArea_chat.append(message.getContent()+"\n\n");
+                jTextArea_chat.append("来自 " + message.getFromIp() + "\n");
+                jTextArea_chat.append(message.getContent() + "\n\n");
 
                 jScrollBar_chat.validate();
                 jScrollBar_chat.setValue(jScrollBar_chat.getMaximum());
 
             }
 
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
 //接收服务器数据
-
 
 
     }
@@ -347,5 +346,80 @@ public class ClientChat {
         // TODO: place custom component creation code here
     }
 
+    {
+// GUI initializer generated by IntelliJ IDEA GUI Designer
+// >>> IMPORTANT!! <<<
+// DO NOT EDIT OR ADD ANY CODE HERE!
+        $$$setupUI$$$();
+    }
+
+    /**
+     * Method generated by IntelliJ IDEA GUI Designer
+     * >>> IMPORTANT!! <<<
+     * DO NOT edit this method OR call it in your code!
+     *
+     * @noinspection ALL
+     */
+    private void $$$setupUI$$$() {
+        panel1 = new JPanel();
+        panel1.setLayout(new GridLayoutManager(6, 2, new Insets(5, 5, 5, 5), -1, -1));
+        panel1.setPreferredSize(new Dimension(600, 400));
+        panel1.setRequestFocusEnabled(false);
+        sendButton = new JButton();
+        sendButton.setText("Send");
+        panel1.add(sendButton, new GridConstraints(5, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label1 = new JLabel();
+        label1.setText("聊天框");
+        panel1.add(label1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        final JLabel label2 = new JLabel();
+        label2.setText("在线列表");
+        panel1.add(label2, new GridConstraints(1, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_FIXED, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        scrollPane1 = new JScrollPane();
+        scrollPane1.setHorizontalScrollBarPolicy(31);
+        panel1.add(scrollPane1, new GridConstraints(2, 0, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        textArea1_chat = new JTextArea();
+        textArea1_chat.setEditable(false);
+        textArea1_chat.setLineWrap(true);
+        textArea1_chat.setMargin(new Insets(0, 0, 0, 0));
+        textArea1_chat.setWrapStyleWord(true);
+        scrollPane1.setViewportView(textArea1_chat);
+        list1 = new JList();
+        panel1.add(list1, new GridConstraints(2, 1, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(150, 50), null, 0, false));
+        Jtoolbar = new JToolBar();
+        panel1.add(Jtoolbar, new GridConstraints(0, 0, 1, 2, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_FIXED, null, new Dimension(-1, 20), null, 0, false));
+        nicknameButton = new JButton();
+        nicknameButton.setText("Nickname");
+        Jtoolbar.add(nicknameButton);
+        final JToolBar.Separator toolBar$Separator1 = new JToolBar.Separator();
+        Jtoolbar.add(toolBar$Separator1);
+        hideIPButton = new JButton();
+        hideIPButton.setText("HideIP");
+        Jtoolbar.add(hideIPButton);
+        final JToolBar.Separator toolBar$Separator2 = new JToolBar.Separator();
+        Jtoolbar.add(toolBar$Separator2);
+        serverIPButton = new JButton();
+        serverIPButton.setText("serverIP");
+        Jtoolbar.add(serverIPButton);
+        final JToolBar.Separator toolBar$Separator3 = new JToolBar.Separator();
+        Jtoolbar.add(toolBar$Separator3);
+        fileShareButton = new JButton();
+        fileShareButton.setText("FileShare");
+        Jtoolbar.add(fileShareButton);
+        jscrollpane_message = new JScrollPane();
+        jscrollpane_message.setHorizontalScrollBarPolicy(31);
+        panel1.add(jscrollpane_message, new GridConstraints(4, 0, 2, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, new Dimension(-1, 40), null, 0, false));
+        textArea_message = new JTextArea();
+        textArea_message.setLineWrap(true);
+        textArea_message.setMargin(new Insets(2, 2, 2, 2));
+        textArea_message.setWrapStyleWord(true);
+        jscrollpane_message.setViewportView(textArea_message);
+    }
+
+    /**
+     * @noinspection ALL
+     */
+    public JComponent $$$getRootComponent$$$() {
+        return panel1;
+    }
 }
 
